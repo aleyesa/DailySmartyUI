888a67809b1edc7828a2c10dafeb269ae9303e10
aleyesa <aaron.leyesa@gmail.com>
2023-10-19 11:51:26 -0700

    put together fetchPostsWithQuery action to receive queried posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,3 +14,16 @@ export function fetchRecentPosts() {
             })
     }
 }
+
+export function fetchPostsWithQuery(query) {
+    return function(dispatch) {
+        axios.get(`https://api.dailysmarty.com/search?q=${query}`)
+            .then(response => {
+                console.log(response.data.posts);
+                // dispatch({
+                //     type:SET_,
+                //     payload: response.data.posts
+                // })
+            })
+    }
+}
